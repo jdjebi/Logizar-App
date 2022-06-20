486bc6391416ac54d115436154ee4692a46fb71c
jdjebi <jeanmarcdjebi@gmail.com>
2022-06-20 18:20:08 +0000

    Ajout du role au utilisateurs

--- database/migrations/2022_06_15_151337_add_role_to_users.php
+++ database/migrations/2022_06_15_151337_add_role_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role',20);
+            $table->string('role',20)->default("user");
         });
     }
 
@@ -29,4 +29,5 @@ return new class extends Migration
             $table->dropColumn('role');
         });
     }
+
 };
