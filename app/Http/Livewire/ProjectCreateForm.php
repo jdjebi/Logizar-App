59cc98379d2d08b5066c6d5574029dc7a7bfa9c7
jdjebi <jeanmarcdjebi@gmail.com>
2022-06-20 18:26:00 +0000

    Support pour la création de projet avec catégorie

--- app/Http/Livewire/ProjectCreateForm.php
+++ app/Http/Livewire/ProjectCreateForm.php
@@ -4,14 +4,23 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use App\Models\Project;
+use App\Models\Category;
+use App\Models\OtherCategory;
 
 class ProjectCreateForm extends Component
 {
+    public $categories;
+
     public $name;
     public $description;
     public $summary;
+
+    public $categorySelectedId;
+    public $otherCategoryContent;
+    public $categoriesSelected = [];
+    public $categoriesTabou = [];
+    public $addCategoryDisabled = false;
 
     protected $rules = [
         'name' => 'required|max:30',
@@ -19,9 +28,70 @@ class ProjectCreateForm extends Component
         'description' => 'required'
     ];
 
+    public function mount(){
+        $this->categories = Category::orderBy("name")->get();
+
+        /*
+        if(count($this->categories) > 0){
+            $this->categorySelectedId = $this->categories[0]->id;
+        }
+        */
+    }
+
+    public function addCategory(){  
+    
+        if(!empty($this->categorySelectedId)){
+
+            if($this->categorySelectedId == "other" and !empty($this->otherCategoryContent)){
+                $otherCategory = new OtherCategory;
+                $otherCategory->name = $this->otherCategoryContent; 
+                $otherCategory->type = "other";              
+                $this->categoriesSelected[] = $otherCategory->toArray();
+            }else{
+                $category = $this->categories->find($this->categorySelectedId);
+                $this->categoriesSelected[] = $category->toArray();
+                $this->categoriesTabou[] = $category->id;
+            }
+
+            if(count($this->categoriesSelected) == 3){
+                $this->addCategoryDisabled = true;
+            }
+
+            $this->reset("categorySelectedId");
+            $this->reset("otherCategoryContent");
+        }
+    }
+
+    public function removeCategory($key){
+        $tkey = array_search($this->categoriesSelected[$key]["id"],$this->categoriesTabou);
+        
+        unset($this->categoriesSelected[$key]);
+
+        if($tkey){
+            unset($this->categoriesTabou[$tkey]);
+        }
+
+        $this->categoriesSelected = array_values($this->categoriesSelected);
+        $this->categoriesTabou = array_values($this->categoriesTabou);
+
+        if(count($this->categoriesSelected) == 2){
+            $this->addCategoryDisabled = false;
+        }
+    }
+
     public function submit()
     { 
+
+        error_log($this->name);
+        error_log($this->description);
+        error_log($this->summary);
+        error_log(json_encode($this->categoriesSelected));
+
+        return;
+
         $this->validate(); 
+
+        // Faire un code de validation des catégories
 
         $project = new Project;
         $project->name = $this->name;
--- resources/views/livewire/project/project-create-form.blade.php
+++ resources/views/livewire/project/project-create-form.blade.php
@@ -5,24 +5,99 @@
     <form wire:submit.prevent="submit">
         @csrf
 
-        <div>
-            <x-jet-label for="name" value="Nom du projet" />
-            <x-jet-input id="name" class="block mt-1 w-full" type="text" wire:model="name" :value="old('name')" />
-            <span class="text-sm text-gray-500">le nom doit posséder 30 caractères</span>
+        <div id="description">
+
+            <div class="text-xl">Description du projet</div>
+
+            <div class="border-b my-3"></div>
+
+            <div>
+                <x-jet-label for="name" value="Nom du projet" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" wire:model="name" :value="old('name')" />
+                <span class="text-sm text-gray-500">le nom doit posséder 30 caractères</span>
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="summary" value="Résumé" />
+                <x-jet-input id="summary" class="block mt-1 w-full" type="text" wire:model="summary"
+                    :value="old('summary')" />
+                <span class="text-sm text-gray-500">le résumé doit posseder 70 caractères</span>
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="description" value="Description" />
+                <textarea
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full"
+                    wire:model="description" id="" cols="30" rows="10"></textarea>
+            </div>
+
         </div>
 
-        <div class="mt-4">
-            <x-jet-label for="summary" value="Résumé" />
-            <x-jet-input id="summary" class="block mt-1 w-full" type="text" wire:model="summary" :value="old('summary')" />
-            <span class="text-sm text-gray-500">le résumé doit posseder 70 caractères</span>
+        <div id="categorize" class="mt-5">
+
+            <div class="text-xl">Catégorisation</div>
+            <div class="border-b my-3"></div>
+            <div>   
+
+                <div>
+                    <x-jet-label for="categorie" value="Catégories du projet" />
+                </div>
+
+                <div class="mt-2">
+                    @foreach ($categoriesSelected as $category)
+                        @if($category['type'] == "system")
+                            <div class="flex items-center mb-2">
+                                <div>
+                                    <x-jet-input name="category-{{ $loop->index }}" type="text" value="{{ $category['name'] }}" class="block mt-1 disabled:bg-gray-100" disabled></x-input>                                        
+                                </div>
+                                <div class="ml-3">
+                                    <x-buttons.btn-red type="button" class="py-3 uppercase" wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
+                                </div>
+                            </div>
+                        @else
+                            <div class="flex items-center mb-2">
+                                <div>
+                                    <x-jet-input type="text" value="Autre ({{ $category['name'] }})" class="block mt-1 disabled:bg-gray-100" disabled></x-input>                                        
+                                </div>
+                                <div class="ml-3">
+                                    <x-buttons.btn-red type="button" class="py-3 uppercase" wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+
+                <div class="flex items-center mt-5">
+
+                    <x-forms.select name="category" class="block mt-1" wire:model="categorySelectedId">
+                        <option value="">Sélectionnez une catégorie</option>
+                        @foreach ($categories as $category)
+                            @if(!in_array($category->id,$categoriesTabou))
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>    
+                            @endif
+                        @endforeach
+                        <option value="other">Autre</option>
+                    </x-forms>
+
+                    @if($categorySelectedId == "other")
+                        <x-jet-input id="otherCategoryContent" class="block mt-1 w-full ml-3" type="text" wire:model.defer="otherCategoryContent"
+                            placeholder="Titre de la catégorie" />
+                    @endif
+
+                    <div class="ml-3">
+                        <x-jet-button type="button" class="py-3" wire:click="addCategory" :disabled="$addCategoryDisabled">Ajouter</x-jet-button>
+                    </div>
+
+                </div>
+
+
+
+            </div>
         </div>
 
-        <div class="mt-4">
-            <x-jet-label for="description" value="Description" />
-            <textarea class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full" wire:model="description" id="" cols="30" rows="10"></textarea>
-        </div>
+        <div class="border-b my-3"></div>
 
-        <div class="mt-4">  
+        <div class="mt-4 text-right">
             <x-jet-button>Enregistrer</x-jet-button>
         </div>
 
