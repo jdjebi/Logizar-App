61377f5ae3ea60f181448cd6292ef650340b99ce
jdjebi <jeanmarcdjebi@gmail.com>
2022-06-24 10:42:29 +0000

    Ajout des méthodes de récupération des catégories

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProjectCategory;
+use Error;
 
 class Project extends Model
 {
+    use HasFactory;
 
     protected $dates = ['created_at'];
 
@@ -15,8 +18,29 @@ class Project extends Model
         'description',
     ];  
     
-    public function user()
-    {
+    public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function categories(){
+        $categories = [];
+
+        $projectCategories = $this->projectCategories()->get();
+
+        foreach($projectCategories as $pcategory){
+            if($pcategory->type == "system"){
+                $category = Category::find($pcategory->category_id);
+            }else if($pcategory->type == "other"){
+                $category = OtherCategory::find($pcategory->category_id);
+            }
+            $categories[] = $category;
+        }
+        
+        return $categories;
+    }
+
+    public function projectCategories(){
+        return $this->hasMany(ProjectCategory::class)->orderBy('created_at');
+    }
+
 }
