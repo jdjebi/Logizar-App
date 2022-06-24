d56864400f783b339ff04c057ff0843457603fc4
jdjebi <jeanmarcdjebi@gmail.com>
2022-06-24 10:46:27 +0000

    Support pour la gestion de l'etat disabled

--- resources/views/components/buttons/btn-light.blade.php
+++ resources/views/components/buttons/btn-light.blade.php
@@ -1,5 +1,7 @@
+@props(['disabled' => false])
+
 @php
-$classes = "px-4 py-2 inline-block text-gray-900 bg-white border border-gray-300 rounded-md focus:outline-none hover:bg-gray-100 focus:ring-4 font-medium  text-sm transition"
+$classes = "px-4 py-2 inline-block text-gray-900 bg-white border border-gray-300 rounded-md focus:outline-none hover:bg-gray-100 focus:ring-4 font-medium  text-sm disabled:opacity-25 transition"
 @endphp
 
-<button {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</button>
+<button {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</button>
