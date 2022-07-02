dbe0b763066c160c43a7bea7ce6e79a24417a229
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-02 09:55:48 +0000

    Support catégories : Correctifs pour la prise en charge du rendu des catégories sélectionnées

--- app/Http/Livewire/Projects/ProjectSimpleCardList.php
+++ app/Http/Livewire/Projects/ProjectSimpleCardList.php
@@ -4,15 +4,47 @@ namespace App\Http\Livewire\Projects;
 
 use Livewire\Component;
 use App\Models\Project;
+use Illuminate\Support\Facades\DB;
 
 class ProjectSimpleCardList extends Component
 {
-    public function render()
-    {
-        $projects = Project::orderByDesc("created_at")->get();
 
-        return view('livewire.projects.project-simple-card-list',[
-            "projects" => $projects
-        ]);
+    public $projects;
+    public $projectsDefault;
+
+    protected $listeners = [
+        'projectsSelectedByCategorizationSidebar' => 'refreshProjects',
+        'ping' => 'test'
+    ];
+
+    public function mount(){
+        $this->projects = $this->getAllProjects();
+        $this->projectsDefault = $this->projects;
     }
+
+
+    public function refreshProjects($keys){
+        if(empty($keys)){
+            $this->projects = $this->projectsDefault;
+        }else{
+            $data = Project::whereIn('id', function($query)  use ($keys){
+                $query->select('projects.id')
+                ->from('projects')
+                ->join("project_category","project_category.project_id","=","projects.id")
+                ->where('project_category.type', "system")
+                ->whereIn('project_category.category_id', $keys)
+                ->orderBy('projects.created_at', 'desc');      
+            })->get();
+            $this->projects = $data;
+        }
+    }
+
+    public function getAllProjects(){
+        return Project::orderByDesc("created_at")->get();
+    }
+
+    public function render(){
+        return view('livewire.projects.project-simple-card-list');
+    }
+    
 }
--- resources/views/livewire/projects/project-simple-card-list.blade.php
+++ resources/views/livewire/projects/project-simple-card-list.blade.php
@@ -1,5 +1,21 @@
-<div class="columns-1 md:columns-3">
-    @foreach ($projects as $project)
-        @livewire('projects.project-simple-card',["project" => $project])
-    @endforeach
-</div>
+@php
+$nbrProjects = count($projects)
+@endphp
+
+@if($nbrProjects < 3)
+
+    <div class="grid grid-cols-1 gap-0 md:grid-cols-2 lg:grid-cols-3 md:gap-3">
+        @foreach ($projects as $project)
+            @livewire('projects.project-simple-card',["project" => $project],key("2-".$project->id))
+        @endforeach
+    </div>
+
+@else
+
+    <div class="columns-3xs">
+        @foreach ($projects as $project)
+            @livewire('projects.project-simple-card',["project" => $project],key($project->id))
+        @endforeach
+    </div>
+
+@endif
