bacf52faf9f2630c1f796307d1ce4d619a8a0c31
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-02 10:14:39 +0000

    Retrait de l'id

--- resources/views/livewire/projects/project-simple-card.blade.php
+++ resources/views/livewire/projects/project-simple-card.blade.php
@@ -6,7 +6,7 @@ $categories = $project->categories();
     <div id="pj-{{ $project->id }}"
         class="break-inside-avoid p-4 mb-3 border box-border border-slate-400 rounded hover:border-blue-500 hover:shadow focus:ring-blue-300 active:ring-blue-300 transition">
         <a href="{{ route('project.show.public', $project->id) }}">
-            <div class="text-xl font-semibold">{{ $project->name }} #{{ $project->id }}</div>
+            <div class="text-xl font-semibold">{{ $project->name }}</div>
             <div class="mt-1">
                 <span class="text-slate-500 text-sm">
                     {{ $project->user->name }} &middot; {{ $project->created_at->format('d/m/Y') }}
