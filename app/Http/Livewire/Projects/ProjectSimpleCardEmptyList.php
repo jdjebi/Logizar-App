e220bef2474a0ba6a2bd8614235a3780aaec2bb5
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-05 13:03:06 +0000

    Composant responsable de l'affichage des resultats de recherche

---
+++ app/Http/Livewire/Projects/ProjectSimpleCardEmptyList.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\Projects;
+
+use Livewire\Component;
+use App\Models\Project;
+
+class ProjectSimpleCardEmptyList extends Component
+{
+
+    public $projects;
+    public $projectsDefault;
+
+    protected $listeners = [
+        'projectsSelectedByCategorizationSidebar' => 'refreshProjects',
+    ];
+
+    public function mount($projects){
+        $this->projects = $projects;
+        $this->projectsDefault = $this->projects;
+    }
+
+    public function refreshProjects($data){
+        $keys = $data["keys"];
+        if(empty($keys)){
+            $this->projects = $this->projectsDefault;
+        }else{
+            $ps = $this->projects;
+            $this->projects = $ps->reject(function($p) use($keys){
+                $pcs = $p->projectCategories()->get();
+                $c_ids = $pcs->map(function($pc){
+                    return $pc->category_id;
+                });
+                $c_ids = $c_ids->toArray();
+                return count(array_intersect($c_ids, $keys)) ? false : true;
+            });
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.projects.project-simple-card-empty-list');
+    }
+}
