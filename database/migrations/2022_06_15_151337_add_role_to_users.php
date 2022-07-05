ca3cf49d635868a19ecd3314729ed74b046929de
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-05 12:50:58 +0000

    Correction de la valeur par défaut du role

--- database/migrations/2022_06_15_151337_add_role_to_users.php
+++ database/migrations/2022_06_15_151337_add_role_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role',20)->default("user");
+            $table->string('role',20)->nullable();
         });
     }
 
