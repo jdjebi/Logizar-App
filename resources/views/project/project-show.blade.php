62d27ee1df4e50578b5aa1657ca7f9084af8b6db
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-08 09:07:44 +0000

    Ajout du composant indiquant que le projet n'a pas de nom de code

--- resources/views/project/project-show.blade.php
+++ resources/views/project/project-show.blade.php
@@ -14,6 +14,12 @@
         </h2>
     </x-slot>
 
+    @auth
+        @if (Auth::user()->id == $project->user->id)
+            <x-project.viewform.plugins.alert-codename-missing :project="$project" />
+        @endif
+    @endauth
+
     @livewire('projects.project-view-form', ["project" => $project])
 
 </x-app-layout>
--- resources/views/project/show-public.blade.php
+++ resources/views/project/show-public.blade.php
@@ -2,12 +2,18 @@
 
     <x-slot name="extras_css">
         <style>
-            .min-h-screen{
+            .min-h-screen {
                 background-color: #fff !important;
             }
         </style>
     </x-slot>
 
-    @livewire('projects.project-view-form', ["project" => $project])
+    @auth
+        @if (Auth::user()->id == $project->user->id)
+            <x-project.viewform.plugins.alert-codename-missing :project="$project" />
+        @endif
+    @endauth
+
+    @livewire('projects.project-view-form', ['project' => $project])
 
 </x-app-layout>
