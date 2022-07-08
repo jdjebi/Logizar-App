6f104a35e0ff5284cd1b3a34b8057828edaf70d0
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-08 09:08:12 +0000

    Ajout de l'indicateur de l'absence du code de projet

--- resources/views/livewire/project-simple-box.blade.php
+++ resources/views/livewire/project-simple-box.blade.php
@@ -1,28 +1,46 @@
 <div>
 
     <div class="p-4">
-        <div class="text-xl font-semibold ">{{ $name }}</div>
+        <div class="text-xl font-semibold flex items-center">
+
+            <span>
+                {{ $name }}
+            </span>
+
+            @empty($project->code_name)
+                <span class="ml-2" title="Problème détecté. Affichez pour constater">
+                    <svg class="flex-shrink-0 w-4 h-4 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </span>
+            @endempty
+        </div>
         <div class="text-slate-500 text-sm">{{ $owner->name }} &middot; {{ $created_at }}</div>
         <div class="mt-3">
             <span class="text-sm">
-                {{ $project->summary ? $project->summary : "Aucun résumé" }}
+                {{ $project->summary ? $project->summary : 'Aucun résumé' }}
             </span>
         </div>
         <div class="mt-2">
-            {{ Str::words($project->description , 40) }}
+            {{ Str::words($project->description, 40) }}
         </div>
         <div class="mt-2">
-            <x-project.category.category-liner :categories="$project->categories()"></x-project>
+            <x-project.category.category-liner :categories="$project->categories()">
+                </x-project>
         </div>
         <div class="mt-4">
-            <x-buttons.light :href="route('project.show',$project_id)" class="text-xs">Afficher</x-buttons.light>
+            <x-buttons.light :href="route('project.show', $project_id)" class="text-xs">Afficher</x-buttons.light>
 
-            <x-buttons.default :href="route('project.update',$project_id)" class="text-xs">Modifier</x-buttons.default>
+            <x-buttons.default :href="route('project.update', $project_id)" class="text-xs">Modifier</x-buttons.default>
 
-            <x-buttons.red wire:click="openConfirmDeletionModal" href="#delete" class="text-xs">Supprimer</x-buttons.red>
+            <x-buttons.red wire:click="openConfirmDeletionModal" href="#delete" class="text-xs">Supprimer
+            </x-buttons.red>
         </div>
     </div>
-    
+
     <hr>
 
     <x-jet-confirmation-modal wire:model="uiConfirmingUserDeletion" maxWidth="md">
@@ -36,7 +54,7 @@
             <x-jet-secondary-button wire:click="$toggle('uiConfirmingUserDeletion')" wire:loading.attr="disabled">
                 Annuler
             </x-jet-secondary-button>
-    
+
             <x-jet-danger-button class="ml-2" wire:click="delete" wire:loading.attr="disabled">
                 Supprimer le projet
             </x-jet-danger-button>
