7fa1ac3f7934cd8244907066068970aa801abfc2
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-15 22:23:49 +0000

    Correction de l'accéssibilité

--- resources/views/project/project-show.blade.php
+++ resources/views/project/project-show.blade.php
@@ -2,17 +2,23 @@
 
     <x-slot name="extras_css">
         <style>
-            .min-h-screen{
+            .min-h-screen {
                 background-color: #fff !important;
             }
         </style>
     </x-slot>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tableau de bord') }} / <a href="{{ route("dashboard") }}" class="text-blue-500">Mes projets</a> / {{ $project->name }}
-        </h2>
-    </x-slot>
+    @auth
+        @if (Auth::user()->id == $project->user->id)
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Tableau de bord') }} / <a href="{{ route('dashboard') }}" class="text-blue-500">Mes projets</a>
+                    / {{ $project->name }}
+                </h2>
+            </x-slot>
+        @endif
+    @endauth
+
 
     @auth
         @if (Auth::user()->id == $project->user->id)
@@ -20,6 +26,6 @@
         @endif
     @endauth
 
-    @livewire('projects.project-view-form', ["project" => $project])
+    @livewire('projects.project-view-form', ['project' => $project])
 
 </x-app-layout>
