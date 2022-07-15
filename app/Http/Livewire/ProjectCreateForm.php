9a9b5d9e4aad6d27a4d75c0dccddf9cca4d11ffb
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-15 22:33:21 +0000

    Mise à jour de la création d'un projet. Ajout de nouveaux champs

--- app/Http/Livewire/ProjectCreateForm.php
+++ app/Http/Livewire/ProjectCreateForm.php
@@ -8,6 +8,10 @@ use App\Models\Project;
 use App\Models\Category;
 use App\Models\OtherCategory;
 use App\Models\ProjectCategory;
+use App\Models\ProjectType;
+use App\Models\Tag;
+use App\Logizar\Project\ProjectStatusList;
+
 use Illuminate\Validation\Validator;
 use Illuminate\Support\Str;
 
@@ -19,6 +23,13 @@ class ProjectCreateForm extends Component
     public $description;
     public $summary;
     public $code_name;
+    public $status;
+    public $type_id;
+    public $deliverable_id;
+    public $is_opensource = false;
+    public $site_url;
+    public $repository_url;
+    public $tags;
 
     public $baseUrl;
     public $codeNameIsUnique = true;
@@ -30,92 +41,123 @@ class ProjectCreateForm extends Component
     public $addCategoryDisabled = false;
     protected $MAX_CATEGORY = 3;
 
+    public $projectTypes = [];
+    public $projectTypesList = [];
+
     protected $rules = [
         'name' => 'required|max:30',
         'code_name' => 'required|regex:/^\w[-.\w]*\w$/|max:45|unique:projects',
         'summary' => 'required|max:70',
-        'description' => 'required'
+        'description' => 'required',
+        'type_id' => 'required',
+        'deliverable_id' => 'required',
+        'site_url' => 'nullable|url',
+        'repository_url' => 'nullable|url'
     ];
 
-    public function mount(){
-        $this->baseUrl = route("project.show.bycodename",[
+    protected $listeners = [
+        "tagsInputChanged" => "updateTags"
+    ];
+
+    public function mount()
+    {
+
+        $this->baseUrl = route("project.show.bycodename", [
             "code_name" => ""
         ]);
+
         $this->categories = Category::orderBy("name")->get();
+        $this->projectTypes = ProjectType::orderBy("name")->get();
+        $this->projectStatusList = ProjectStatusList::STATUS_LIST;
+
+        if (!empty($this->projectTypes)) {
+            $this->type_id = $this->projectTypes[0]->id;
+        }
+
+        if (!empty($this->projectStatusList)) {
+            $this->status = $this->projectStatusList["in_progress"]["name"];
+        }
     }
 
-    public function addCategory(){  
-    
-        if(!empty($this->categorySelectedId)){
+    public function addCategory()
+    {
 
-            if($this->categorySelectedId == "other"){
-                if(!empty($this->otherCategoryContent)){
+        if (!empty($this->categorySelectedId)) {
+
+            if ($this->categorySelectedId == "other") {
+                if (!empty($this->otherCategoryContent)) {
                     $otherCategory = new OtherCategory;
-                    $otherCategory->id = null; 
-                    $otherCategory->name = $this->otherCategoryContent; 
-                    $otherCategory->type = "other";              
+                    $otherCategory->id = null;
+                    $otherCategory->name = $this->otherCategoryContent;
+                    $otherCategory->type = "other";
                     $this->categoriesSelected[] = $otherCategory->toArray();
                 }
-            }else{
+            } else {
                 $category = $this->categories->find($this->categorySelectedId);
                 $this->categoriesSelected[] = $category->toArray();
                 $this->categoriesTabou[] = $category->id;
             }
 
-            if(count($this->categoriesSelected) == $this->MAX_CATEGORY){
+            if (count($this->categoriesSelected) == $this->MAX_CATEGORY) {
                 $this->addCategoryDisabled = true;
             }
 
             $this->reset("categorySelectedId");
             $this->reset("otherCategoryContent");
-
         }
     }
 
-    public function removeCategory($key){
-        $tkey = array_search($this->categoriesSelected[$key]["id"],$this->categoriesTabou);
-        
+    public function updateTags($tags)
+    {
+        $this->tags = $tags["data"];
+    }
+
+    public function removeCategory($key)
+    {
+        $tkey = array_search($this->categoriesSelected[$key]["id"], $this->categoriesTabou);
+
         unset($this->categoriesSelected[$key]);
 
-        if($tkey){
+        if ($tkey) {
             unset($this->categoriesTabou[$tkey]);
         }
 
         $this->categoriesSelected = array_values($this->categoriesSelected);
         $this->categoriesTabou = array_values($this->categoriesTabou);
 
-        if(count($this->categoriesSelected) == 2){
+        if (count($this->categoriesSelected) == 2) {
             $this->addCategoryDisabled = false;
         }
-        
     }
 
-    public function generateCodeName(){
-        if(empty($this->name)){
+    public function generateCodeName()
+    {
+        if (empty($this->name)) {
             $this->code_name = "";
-        }else{
-            $this->code_name = Str::slug($this->name,".");
+        } else {
+            $this->code_name = Str::slug($this->name, ".");
         }
         $this->checkCodeNameUnicity();
     }
 
-    public function checkCodeNameUnicity(){
+    public function checkCodeNameUnicity()
+    {
         // Verifier si le code est unique
-        $result = Project::where("code_name",$this->code_name)->count(); 
-        if($result==0){
+        $result = Project::where("code_name", $this->code_name)->count();
+        if ($result == 0) {
             // Cette condition supplémentaire permet d'éviter la mise à jour su la valeur n'a pas changé
-            if($this->codeNameIsUnique == false){
+            if ($this->codeNameIsUnique == false) {
                 $this->codeNameIsUnique = true;
             }
-        }else{
-            if($this->codeNameIsUnique == true){
+        } else {
+            if ($this->codeNameIsUnique == true) {
                 $this->codeNameIsUnique = false;
             }
         }
     }
 
     public function submit()
-    { 
+    {
 
         /*
             Cette methode de validation permet de valider les categories car elles ne sont pas champs du composant.
@@ -131,26 +173,37 @@ class ProjectCreateForm extends Component
         $this->withValidator(function (Validator $validator) {
 
             $validator->after(function ($validator) {
-                   
-                if(count($this->categoriesSelected) == 0){
+
+                if (count($this->categoriesSelected) == 0) {
 
                     $validator->errors()->add('categories.empty', 'Veuillez renseigner au moins une catégorie.');
 
-                    foreach($this->categoriesSelected as $category){
-                        
-                        if($category->type == "other" && !empty($category->name)){
+                    foreach ($this->categoriesSelected as $category) {
+
+                        if ($category->type == "other" && !empty($category->name)) {
 
                             $validator->errors()->add('categories.other.empty', 'Une catégorie autre ne possède pas de titre. Veuillez en renseigner une.');
-
                         }
-                        
                     }
-
                 }
 
+                if (count($validator->errors()) > 0) {
+                    $this->dispatchBrowserEvent('alert', [
+                        'type' => 'error',
+                        'message' => "Le formulaire comporte des erreurs"
+                    ]);
+                }
             });
-
         })->validate();
+
+        // Preparation de l'enregistrement
+        if ($this->type_id == "other") {
+            $this->type_id = null;
+        }
+
+        if ($this->deliverable_id == "other") {
+            $this->deliverable_id = null;
+        }
 
         // Enregistrement du projet
         $project = new Project;
@@ -159,12 +212,16 @@ class ProjectCreateForm extends Component
         $project->summary = $this->summary;
         $project->code_name = $this->code_name;
         $project->user_id = Auth::id();
+        $project->status = $this->status;
+        $project->is_opensource = $this->is_opensource;
+        $project->type_id = $this->type_id;
+        $project->deliverable_id = $this->deliverable_id;
+        $project->site_url = $this->site_url;
+        $project->repository_url = $this->repository_url;
         $project->save();
 
         // Enregistrement de la categorisation
         foreach($this->categoriesSelected as $category){
-
-            error_log($category["type"]);
 
             if($category["type"] == "other"){
 
@@ -192,14 +249,23 @@ class ProjectCreateForm extends Component
             }
 
         }
+
+        // Enregistrement des tags
+        foreach($this->tags as $t){
+            $tag = new Tag;
+            $tag->name = $t;
+            $tag->project_id = $project->id;
+            $tag->save();
+        }
         
         // Notification
         session()->flash('flash.banner', 'Projet créé !');
         session()->flash('flash.bannerStyle', 'success');
 
         return redirect()->route("project.show.bycodename",$project->code_name);
+
     }
-    
+
     public function render()
     {
         return view('livewire.project.project-create-form');
--- resources/views/livewire/project/project-create-form.blade.php
+++ resources/views/livewire/project/project-create-form.blade.php
@@ -12,14 +12,14 @@
             <div class="border-b my-3"></div>
 
             <div>
-                <x-jet-label for="name" value="Nom du projet" />
+                <x-jet-label for="name" value="Nom du projet*" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" wire:model="name"
                     wire:input="generateCodeName" :value="old('name')" />
                 <span class="text-sm text-gray-500">30 caractères au maximum.</span>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="code_name" value="Code du projet" title="Nom du projet utilisé dans son url" />
+                <x-jet-label for="code_name" value="Code du projet*" title="Nom du projet utilisé dans son url" />
                 <x-jet-input id="code_name" class="block mt-1 w-full" type="text" wire:model="code_name"
                     wire:input='checkCodeNameUnicity' :value="old('code_name')" />
                 <div class="my-3">
@@ -40,93 +40,146 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="summary" value="Résumé" />
+                <x-jet-label for="summary" value="Résumé*" />
                 <x-jet-input id="summary" class="block mt-1 w-full" type="text" wire:model="summary"
                     :value="old('summary')" />
                 <span class="text-sm text-gray-500">70 caractères au maximum.</span>
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="description" value="Description" />
+                <x-jet-label for="description" value="Description*" />
                 <textarea
                     class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm block mt-1 w-full"
                     wire:model="description" id="" cols="30" rows="10"></textarea>
             </div>
 
+            <div class="mt-4">
+                <x-jet-label for="status" value="Statut du projet*" />
+                <x-forms.select name="status" class="block mt-1" :value="old('status')">
+                    @foreach ($projectStatusList as $status)
+                        <option value="{{ $status["name"] }}">{{  $status["label"] }}</option>
+                    @endforeach
+                </x-forms.select>
+            </div>
+
         </div>
 
         <div id="categorize" class="mt-5">
-
             <div class="text-xl">Catégorisation</div>
             <div class="border-b my-3"></div>
             <div>
-
                 <div>
-                    <x-jet-label for="categorie" value="Catégories du projet" />
-                </div>
 
-                <div class="flex items-center mt-2">
-
-                    <x-forms.select name="category" class="block mt-1" wire:model="categorySelectedId">
-                        <option value="">Sélectionnez une catégorie</option>
-                        @foreach ($categories as $category)
-                            @if (!in_array($category->id, $categoriesTabou))
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endif
-                        @endforeach
-                        <option value="other">Autre</option>
-                        </x-forms>
-
+                    <div>
+                        <x-jet-label for="category" value="Catégories du projet*" />
+                    </div>
+                    <div class="flex items-center mt-2">
+                        <x-forms.select name="category" class="block mt-1" wire:model="categorySelectedId">
+                            <option value="">Sélectionnez une catégorie</option>
+                            @foreach ($categories as $category)
+                                @if (!in_array($category->id, $categoriesTabou))
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endif
+                            @endforeach
+                            <option value="other">Autre</option>
+                        </x-forms.select>
                         @if ($categorySelectedId == 'other')
                             <x-jet-input id="otherCategoryContent" class="block mt-1 w-full ml-3" type="text"
                                 wire:model.defer="otherCategoryContent" placeholder="Titre de la catégorie" />
                         @endif
-
                         <div class="ml-3">
                             <x-jet-button type="button" class="py-3" wire:click="addCategory" :disabled="$addCategoryDisabled">
                                 Ajouter</x-jet-button>
                         </div>
-
+                    </div>
+                    <div class="mt-2">
+                        @foreach ($categoriesSelected as $category)
+                            @if ($category['type'] == 'system')
+                                <div class="flex items-center mb-2">
+                                    <div>
+                                        <x-jet-input name="category-{{ $loop->index }}" type="text"
+                                            value="{{ $category['name'] }}" class="block mt-1 disabled:bg-gray-100"
+                                            disabled>
+                                            </x-input>
+                                    </div>
+                                    <div class="ml-3">
+                                        <x-buttons.btn-red type="button" class="py-3 uppercase"
+                                            wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
+                                    </div>
+                                </div>
+                            @else
+                                <div class="flex items-center mb-2">
+                                    <div>
+                                        <x-jet-input type="text" value="Autre ({{ $category['name'] }})"
+                                            class="block mt-1 disabled:bg-gray-100" disabled>
+                                            </x-input>
+                                    </div>
+                                    <div class="ml-3">
+                                        <x-buttons.btn-red type="button" class="py-3 uppercase"
+                                            wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
                 </div>
-
-                <div class="mt-2">
-                    @foreach ($categoriesSelected as $category)
-                        @if ($category['type'] == 'system')
-                            <div class="flex items-center mb-2">
-                                <div>
-                                    <x-jet-input name="category-{{ $loop->index }}" type="text"
-                                        value="{{ $category['name'] }}" class="block mt-1 disabled:bg-gray-100"
-                                        disabled>
-                                        </x-input>
-                                </div>
-                                <div class="ml-3">
-                                    <x-buttons.btn-red type="button" class="py-3 uppercase"
-                                        wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
-                                </div>
-                            </div>
-                        @else
-                            <div class="flex items-center mb-2">
-                                <div>
-                                    <x-jet-input type="text" value="Autre ({{ $category['name'] }})"
-                                        class="block mt-1 disabled:bg-gray-100" disabled>
-                                        </x-input>
-                                </div>
-                                <div class="ml-3">
-                                    <x-buttons.btn-red type="button" class="py-3 uppercase"
-                                        wire:click="removeCategory({{ $loop->index }})">Retirer</x-jet-button>
-                                </div>
-                            </div>
-                        @endif
-                    @endforeach
+                <div class="mt-5">
+                    <x-jet-label for="tagsInput" value="Tags" />
+                    <div class="mt-1">
+                        @livewire('forms.tags-input')
+                    </div>
                 </div>
+            </div>
+        </div>
 
+        <div id="specification" class="mt-5">
+            <div class="text-xl">Spécifications</div>
+            <div class="border-b my-3"></div>
+            <div>
+                <div class="mt-4">
+                    <x-jet-label for="type_id" value="Type de projet*" />
+                    <x-forms.select name="type_id" class="block mt-1" wire:model='type_id' :value="old('type_id')">
+                        @foreach ($projectTypes as $types)
+                            <option value="{{ $types->id }}">{{ empty($types->shortname) ? $types->name : $types->shortname }}</option>
+                        @endforeach
+                        <option value="other">Autre</option>
+                    </x-forms.select>
+                </div>
+                <div class="mt-4">
+                    <x-jet-label for="deliverable_id" value="Livrable*" />
+                    <x-forms.select name="deliverable_id" class="block mt-1" wire:model='deliverable_id' :value="old('deliverable_id')">
+                        <option value="">Sélectionnez un livrable</option>
+                        <option value="other">Autre</option>
+                    </x-forms.select>
+                </div>
+                <div class="mt-4">
+                    <x-forms.checkbox id="is_opensource" class="mb-4" model='is_opensource' :value="old('is_opensource')"
+                        message="Projet opensource" />
+                </div>
+            </div>
+        </div>
+
+        <div id="accessibility" class="mt-5">
+            <div class="text-xl">Accessibilité</div>
+            <div class="border-b my-3"></div>
+            <div>
+                <div>
+                    <x-jet-label for="site_url" value="Site web" />
+                    <x-jet-input id="site_url" class="block mt-1 w-full" type="text" wire:model='site_url' :value="old('site_url')" />
+                    <span class="text-sm text-gray-500">100 caractères au maximum.</span>
+                </div>
+                <div class="mt-4">
+                    <x-jet-label for="repository_url" value="Lien du dépôt" />
+                    <x-jet-input id="repository_url" class="block mt-1 w-full" type="text" wire:model='repository_url' :value="old('repository_url')" />
+                    <span class="text-sm text-gray-500">100 caractères au maximum.</span>
+                </div>
             </div>
         </div>
 
         <div class="border-b my-3"></div>
 
         <div class="mt-4 text-right">
-            <x-jet-button>Enregistrer</x-jet-button>
+            <x-jet-button type="submit">Enregistrer</x-jet-button>
         </div>
 
     </form>
