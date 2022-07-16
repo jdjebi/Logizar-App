db130fa415e1f3979628930000fd195bfaad63f8
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-16 19:53:04 +0000

    Ajout des méthodes d'accès au statut et au type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\ProjectCategory;
+use App\Logizar\Project\ProjectStatusList;
+
 use Error;
 
 class Project extends Model
@@ -15,9 +16,29 @@ class Project extends Model
 
     protected $guarded = [];
 
+    public function statusFull()
+    {
+        if(isset(ProjectStatusList::STATUS_LIST[$this->status])){
+            $status = ProjectStatusList::STATUS_LIST[$this->status];
+        }else{
+            $status = null;
+        }
+        return $status;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(ProjectType::class);
+    }
+
+    public function deliverable()
+    {
+        return $this->belongsTo(ProjectDeliverable::class);
     }
 
     public function tags()
@@ -48,25 +69,24 @@ class Project extends Model
         return $this->hasMany(ProjectCategory::class)->orderBy('created_at');
     }
 
-    public function makeLinkClickable($link, $class='', $target='_blank')
+    public function makeLinkClickable($link, $class = '', $target = '_blank')
     {
         if ($target) {
             $target = ' target="' . $target . '"';
-        }else {
+        } else {
             $target = '';
         }
 
-        if($class){
-            $class="class=\"$class\"";
+        if ($class) {
+            $class = "class=\"$class\"";
         }
 
         $pattern = '/((https?:\/\/)?([-\w]+\.[-\w\.]+)+\w(:\d+)?(\/([-\w\/_\.~]*(\?\S+)?)?)*)/';
 
         //$linkClickable = preg_replace($pattern, '<a href="$1" '.$target.' '.$class.'>$1</a>', $link);
 
-        $linkClickable = preg_replace($pattern, '<a href="$1" '.$target.' '.$class.'>$1</a>', $link);
+        $linkClickable = preg_replace($pattern, '<a href="$1" ' . $target . ' ' . $class . '>$1</a>', $link);
 
         return $linkClickable;
     }
-
 }
