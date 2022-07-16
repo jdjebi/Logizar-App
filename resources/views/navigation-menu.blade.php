dcc31a2a7563c22c951b85cc05d374ab92f5e864
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-16 19:52:13 +0000

    Ajout d'une icone

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -48,7 +48,7 @@
                 @auth
                     <div class="hidden space-x-8 sm:-my-px sm:flex">
                         <x-jet-nav-link href="{{ route('project.create') }}" :active="request()->routeIs('project.create')">
-                            {{ __('Nouveau projet') }}
+                            <i class="fa-solid fa-plus"></i>  &nbsp;&nbsp;{{ __('Nouveau projet') }}
                         </x-jet-nav-link>
                     </div>
 
@@ -65,7 +65,7 @@
                                     @else
                                         <span class="inline-flex rounded-md">
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
-                                                {{ Auth::user()->name }}
+                                               {{ Auth::user()->name }}
 
                                                 <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
