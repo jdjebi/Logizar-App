66ab39ea499065a4c14f071288a7edc2b06d91d7
jdjebi <jeanmarcdjebi@gmail.com>
2022-07-26 16:23:12 +0000

    Revision du code

--- resources/views/livewire/projects/update/project-update-categorization.blade.php
+++ resources/views/livewire/projects/update/project-update-categorization.blade.php
@@ -11,49 +11,20 @@
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
             <div class="text-2xl font-semibold">Catégorisation</div>
-            <div class="mt-4">
-                <div class="text-lg mb-3">Catégories</div>
-            </div>
-            <div class="mb-2 text-sm pl-3">
-                @foreach ($categoriesSelected as $category)
-                    <div class="mb-1">
-                        <a wire:click="removeCategory({{ $loop->index }})" href="javascript:void(0)"
-                            class="active:focus:bg-red-300 inline-flex items-center p-1 mr-2 text-sm font-semibold text-red-800 bg-red-100 rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M20 12H4" />
-                            </svg>
-                        </a>
-                        @if ($category['type'] == 'system')
-                            <span>{{ $category['name'] }}</span>
-                        @else
-                            <span>Autre({{ $category['name'] }})</span>
-                        @endif
-                    </div>
-                @endforeach
-            </div>
+        </div>
+        <div class="col-span-6 sm:col-span-4">
+
             <div>
-                <div class="flex items-center">
-                    <x-forms.select name="category" class="block mt-1" wire:model="categorySelectedId">
-                        <option value="">Sélectionnez une catégorie</option>
-                        @foreach ($categories as $category)
-                            @if (!in_array($category->id, $categoriesTabou))
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endif
-                        @endforeach
-                        <option value="other">Autre</option>
-                    </x-forms.select>
-                    @if ($categorySelectedId == 'other')
-                        <x-jet-input id="otherCategoryContent" class="block mt-1 w-full ml-3" type="text"
-                            wire:model.defer="otherCategoryContent" placeholder="Titre de la catégorie" />
-                    @endif
+                <div class="mb-2">
+                    <x-jet-label for="category" value="Catégories du projet*" />
                 </div>
-                <div class="mt-5">
-                    <x-jet-button type="button" class="py-2 px-2.5" wire:click="addCategory" :disabled="$addCategoryDisabled">Ajouter</x-jet-button>
-                </div>
+                @livewire('projects.forms.categories-select-manager', ['categories_selected' => $categories_selected])
             </div>
+
             <div class="mt-7">
-                <div class="text-lg mb-3">Tags</div>
+                <div class="mb-2">
+                    <x-jet-label for="category" value="Tags" />
+                </div>
                 <div class="mt-1">
                     @livewire('forms.tags-input',[
                         "tags" => $tags
@@ -64,7 +35,7 @@
     </x-slot>
 
     <x-slot name="actions">
-        <x-buttons.btn-light type="button" class="uppercase" wire:click='resetForm' :disabled="!$isFormEdited">Annuler</x-buttons.btn-light>
+        <x-buttons.btn-light type="button" class="uppercase" wire:click='resetForm' :disabled="!$is_form_edited">Annuler</x-buttons.btn-light>
         &nbsp;
         <x-jet-button>Enregistrer</x-jet-button>
     </x-slot>
