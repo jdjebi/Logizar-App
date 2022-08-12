0f4ed0754912ed0820dc6abd12dcf1c2419412bc
jdjebi <jeanmarcdjebi@gmail.com>
2022-08-12 07:13:10 +0000

    Mise à jour

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function admin(){
+        return redirect()->route("admin.project.categories");
+    }
+
     public function adminCategory(){
         return view("admin.categorization.categories");
     }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Project;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Logizar\Search\SearchEngine;
 
-Route::get('/', function () {
+Route::get('/', 'App\Http\Controllers\IndexController')->name('index');
 
-    $projects = Project::orderByDesc("created_at")->get();
-
-    return view('welcome', [
-        "projects" => $projects
-    ]);
-
-})->name('index');
-
-
-Route::get('/search','App\Http\Controllers\Search\SearchController@basic')->name('search.results');
-Route::get('/public/projects/{id}', 'App\Http\Controllers\ProjectController@showPublic')->name('project.show.public');
+Route::get('/search', 'App\Http\Controllers\Search\SearchController@basic')->name('search.results');
+Route::get('/projects/{id}', 'App\Http\Controllers\ProjectController@showById')->where('id', '[0-9]+')->name('project.show.byid');
 Route::get('/p/{code_name?}', 'App\Http\Controllers\ProjectController@showByCodeName')->name('project.show.bycodename');
 
 Route::middleware([
@@ -27,29 +14,19 @@ Route::middleware([
     'verified'
 ])->group(function () {
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
 
     Route::get('/projects/new', 'App\Http\Controllers\ProjectController@create')->name('project.create');
-    Route::post('/projects/new', 'App\Http\Controllers\ProjectController@store');
-    Route::get('/projects/{id}', 'App\Http\Controllers\ProjectController@show')->name('project.show');
     Route::get('/projects/{id}/update', 'App\Http\Controllers\ProjectController@update')->name('project.update');
 
     Route::middleware(['admin'])->group(function () {
-        Route::get('/admin', 'App\Http\Controllers\Admin\AdminController@adminCategory')->name('admin.index');
+
+        Route::get('/admin', 'App\Http\Controllers\Admin\AdminController@admin')->name('admin.index');
         Route::get('/admin/projects/categories', 'App\Http\Controllers\Admin\AdminController@adminCategory')->name('admin.project.categories');
         Route::get('/admin/projects/other-categories', 'App\Http\Controllers\Admin\AdminController@adminOtherCategories')->name('admin.project.other-categories');
         Route::get('/admin/projects/types', 'App\Http\Controllers\Admin\AdminController@adminProjectTypes')->name('admin.project.types');
-        Route::get('/admin/projects/deliverables','App\Http\Controllers\Admin\AdminController@adminProjectDeliverables')->name('admin.project.deliverables');
-    });
-});
+        Route::get('/admin/projects/deliverables', 'App\Http\Controllers\Admin\AdminController@adminProjectDeliverables')->name('admin.project.deliverables');
 
-Route::get("tests/search", function (Request $request) {
-    $searchContent = $request->q;
-    if(!empty($searchContent)){
-        $searchEngine = new SearchEngine();  
-        $results = $searchEngine->searchWithLikeAndOrderByFulltext($searchContent);
-    }
-    return $results;
+        Route::get("tests/search", 'App\Http\Controllers\Search\SearchController@test')->name('admin.search.test');
+    });
 });
